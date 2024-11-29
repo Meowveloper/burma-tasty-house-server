5f51cac3a6fda24a26d2fb0d2b35d26ab148d1f8
Meowveloper <meowveloper.dev@gmail.com>
2024-11-29 21:09:51 +0700

    cloudinary functions

--- src/helpers/deleteFileFromCloudinary.ts
+++ src/helpers/deleteFileFromCloudinary.ts
@@ -1,5 +1,4 @@
 import { v2 as cloudinary } from "cloudinary";
-import path from "path";
 import EnumCloudinaryFileTypes from "../types/EnumCloudinaryFileTypes";
 import extractPublicIdFromCloudinarySecureUrl from "./extractPublicIdFromCloudinarySecureUrl";
 
